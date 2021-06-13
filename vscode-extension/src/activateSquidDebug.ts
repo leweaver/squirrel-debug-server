6413842d9e8fe99607d68391d7538adf87ce5bbe
lewis weaver <ransico@gmail.com>
2021-06-13 16:54:13 -0700

    convert tabs to spaces

--- vscode-extension/src/activateSquidDebug.ts
+++ vscode-extension/src/activateSquidDebug.ts
@@ -10,117 +10,117 @@ import { SquidDebugSession } from './squidDebug';
 import { FileAccessor } from './squidRuntime';
 
 export function activateSquidDebug(context: vscode.ExtensionContext, factory?: vscode.DebugAdapterDescriptorFactory) {
-	context.subscriptions.push(
-		vscode.commands.registerCommand('extension.squirrel-debug.runEditorContents', (resource: vscode.Uri) => {
-			let targetResource = resource;
-			if (!targetResource && vscode.window.activeTextEditor) {
-				targetResource = vscode.window.activeTextEditor.document.uri;
-			}
-			if (targetResource) {
-				vscode.debug.startDebugging(undefined, {
-						type: 'squirrel',
-						name: 'Run File',
-						request: 'launch',
-						program: targetResource.fsPath
-					},
-					{ noDebug: true }
-				);
-			}
-		}),
-		vscode.commands.registerCommand('extension.squirrel-debug.debugEditorContents', (resource: vscode.Uri) => {
-			let targetResource = resource;
-			if (!targetResource && vscode.window.activeTextEditor) {
-				targetResource = vscode.window.activeTextEditor.document.uri;
-			}
-			if (targetResource) {
-				vscode.debug.startDebugging(undefined, {
-					type: 'squirrel',
-					name: 'Debug File',
-					request: 'launch',
-					program: targetResource.fsPath
-				});
-			}
-		}),
-		vscode.commands.registerCommand('extension.squirrel-debug.toggleFormatting', (variable) => {
-			const ds = vscode.debug.activeDebugSession;
-			if (ds) {
-				ds.customRequest('toggleFormatting');
-			}
-		})
-	);
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.squirrel-debug.runEditorContents', (resource: vscode.Uri) => {
+            let targetResource = resource;
+            if (!targetResource && vscode.window.activeTextEditor) {
+                targetResource = vscode.window.activeTextEditor.document.uri;
+            }
+            if (targetResource) {
+                vscode.debug.startDebugging(undefined, {
+                        type: 'squirrel',
+                        name: 'Run File',
+                        request: 'launch',
+                        program: targetResource.fsPath
+                    },
+                    { noDebug: true }
+                );
+            }
+        }),
+        vscode.commands.registerCommand('extension.squirrel-debug.debugEditorContents', (resource: vscode.Uri) => {
+            let targetResource = resource;
+            if (!targetResource && vscode.window.activeTextEditor) {
+                targetResource = vscode.window.activeTextEditor.document.uri;
+            }
+            if (targetResource) {
+                vscode.debug.startDebugging(undefined, {
+                    type: 'squirrel',
+                    name: 'Debug File',
+                    request: 'launch',
+                    program: targetResource.fsPath
+                });
+            }
+        }),
+        vscode.commands.registerCommand('extension.squirrel-debug.toggleFormatting', (variable) => {
+            const ds = vscode.debug.activeDebugSession;
+            if (ds) {
+                ds.customRequest('toggleFormatting');
+            }
+        })
+    );
 
-	context.subscriptions.push(vscode.commands.registerCommand('extension.squirrel-debug.getHostnamePort', config => {
-		return vscode.window.showInputBox({
-			placeHolder: "Please enter the hostname and port of the application hosting Squid",
-			value: "localhost:8000"
-		});
-	}));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.squirrel-debug.getHostnamePort', config => {
+        return vscode.window.showInputBox({
+            placeHolder: "Please enter the hostname and port of the application hosting Squid",
+            value: "localhost:8000"
+        });
+    }));
 
-	// register a configuration provider for current open 'squirrel' file debug type
-	//const provider = new SquidConfigurationProvider();
-	//context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('squirrel', provider));
+    // register a configuration provider for current open 'squirrel' file debug type
+    //const provider = new SquidConfigurationProvider();
+    //context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('squirrel', provider));
 
-	// register a dynamic configuration provider for 'squirrel' debug type
-	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('squirrel', {
-		provideDebugConfigurations(folder: WorkspaceFolder | undefined): ProviderResult<DebugConfiguration[]> {
-			return [
-				{
-					name: "Dynamic Launch",
-					request: "launch",
-					type: "squirrel",
-					program: "${file}"
-				}
-			];
-		}
-	}, vscode.DebugConfigurationProviderTriggerKind.Dynamic));
+    // register a dynamic configuration provider for 'squirrel' debug type
+    context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('squirrel', {
+        provideDebugConfigurations(folder: WorkspaceFolder | undefined): ProviderResult<DebugConfiguration[]> {
+            return [
+                {
+                    name: "Dynamic Launch",
+                    request: "launch",
+                    type: "squirrel",
+                    program: "${file}"
+                }
+            ];
+        }
+    }, vscode.DebugConfigurationProviderTriggerKind.Dynamic));
 
-	if (!factory) {
-		factory = new InlineDebugAdapterFactory();
-	}
-	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('squirrel', factory));
-	if ('dispose' in factory) {
-		context.subscriptions.push(factory);
-	}
+    if (!factory) {
+        factory = new InlineDebugAdapterFactory();
+    }
+    context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('squirrel', factory));
+    if ('dispose' in factory) {
+        context.subscriptions.push(factory);
+    }
 
-	// override VS Code's default implementation of the debug hover
-	context.subscriptions.push(vscode.languages.registerEvaluatableExpressionProvider('squirrel', {
-		provideEvaluatableExpression(document: vscode.TextDocument, position: vscode.Position): vscode.ProviderResult<vscode.EvaluatableExpression> {
-			const wordRange = document.getWordRangeAtPosition(position);
-			return wordRange ? new vscode.EvaluatableExpression(wordRange) : undefined;
-		}
-	}));
+    // override VS Code's default implementation of the debug hover
+    context.subscriptions.push(vscode.languages.registerEvaluatableExpressionProvider('squirrel', {
+        provideEvaluatableExpression(document: vscode.TextDocument, position: vscode.Position): vscode.ProviderResult<vscode.EvaluatableExpression> {
+            const wordRange = document.getWordRangeAtPosition(position);
+            return wordRange ? new vscode.EvaluatableExpression(wordRange) : undefined;
+        }
+    }));
 
-	// override VS Code's default implementation of the "inline values" feature"
-	context.subscriptions.push(vscode.languages.registerInlineValuesProvider('squirrel', {
+    // override VS Code's default implementation of the "inline values" feature"
+    context.subscriptions.push(vscode.languages.registerInlineValuesProvider('squirrel', {
 
-		provideInlineValues(document: vscode.TextDocument, viewport: vscode.Range, context: vscode.InlineValueContext) : vscode.ProviderResult<vscode.InlineValue[]> {
+        provideInlineValues(document: vscode.TextDocument, viewport: vscode.Range, context: vscode.InlineValueContext) : vscode.ProviderResult<vscode.InlineValue[]> {
 
-			const allValues: vscode.InlineValue[] = [];
+            const allValues: vscode.InlineValue[] = [];
 
-			for (let l = viewport.start.line; l <= context.stoppedLocation.end.line; l++) {
-				const line = document.lineAt(l);
-				var regExp = /local_[ifso]/ig;	// match variables of the form local_i, local_f, Local_i, LOCAL_S...
-				do {
-					var m = regExp.exec(line.text);
-					if (m) {
-						const varName = m[0];
-						const varRange = new vscode.Range(l, m.index, l, m.index + varName.length);
+            for (let l = viewport.start.line; l <= context.stoppedLocation.end.line; l++) {
+                const line = document.lineAt(l);
+                var regExp = /local_[ifso]/ig;	// match variables of the form local_i, local_f, Local_i, LOCAL_S...
+                do {
+                    var m = regExp.exec(line.text);
+                    if (m) {
+                        const varName = m[0];
+                        const varRange = new vscode.Range(l, m.index, l, m.index + varName.length);
 
-						// some literal text
-						//allValues.push(new vscode.InlineValueText(varRange, `${varName}: ${viewport.start.line}`));
+                        // some literal text
+                        //allValues.push(new vscode.InlineValueText(varRange, `${varName}: ${viewport.start.line}`));
 
-						// value found via variable lookup
-						allValues.push(new vscode.InlineValueVariableLookup(varRange, varName, false));
+                        // value found via variable lookup
+                        allValues.push(new vscode.InlineValueVariableLookup(varRange, varName, false));
 
-						// value determined via expression evaluation
-						//allValues.push(new vscode.InlineValueEvaluatableExpression(varRange, varName));
-					}
-				} while (m);
-			}
+                        // value determined via expression evaluation
+                        //allValues.push(new vscode.InlineValueEvaluatableExpression(varRange, varName));
+                    }
+                } while (m);
+            }
 
-			return allValues;
-		}
-	}));
+            return allValues;
+        }
+    }));
 }
 
 // class MockConfigurationProvider implements vscode.DebugConfigurationProvider {
@@ -154,28 +154,28 @@ export function activateSquidDebug(context: vscode.ExtensionContext, factory?: v
 // }
 
 export const workspaceFileAccessor: FileAccessor = {
-	async readFile(path: string) {
-		try {
-			const uri = vscode.Uri.file(path);
-			const bytes = await vscode.workspace.fs.readFile(uri);
-			const contents = Buffer.from(bytes).toString('utf8');
-			return contents;
-		} catch(e) {
-			try {
-				const uri = vscode.Uri.parse(path);
-				const bytes = await vscode.workspace.fs.readFile(uri);
-				const contents = Buffer.from(bytes).toString('utf8');
-				return contents;
-			} catch (e) {
-				return `cannot read '${path}'`;
-			}
-		}
-	}
+    async readFile(path: string) {
+        try {
+            const uri = vscode.Uri.file(path);
+            const bytes = await vscode.workspace.fs.readFile(uri);
+            const contents = Buffer.from(bytes).toString('utf8');
+            return contents;
+        } catch(e) {
+            try {
+                const uri = vscode.Uri.parse(path);
+                const bytes = await vscode.workspace.fs.readFile(uri);
+                const contents = Buffer.from(bytes).toString('utf8');
+                return contents;
+            } catch (e) {
+                return `cannot read '${path}'`;
+            }
+        }
+    }
 };
 
 class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory {
 
-	createDebugAdapterDescriptor(_session: vscode.DebugSession): ProviderResult<vscode.DebugAdapterDescriptor> {
-		return new vscode.DebugAdapterInlineImplementation(new SquidDebugSession(workspaceFileAccessor));
-	}
+    createDebugAdapterDescriptor(_session: vscode.DebugSession): ProviderResult<vscode.DebugAdapterDescriptor> {
+        return new vscode.DebugAdapterInlineImplementation(new SquidDebugSession(workspaceFileAccessor));
+    }
 }
--- vscode-extension/src/squidRuntime.ts
+++ vscode-extension/src/squidRuntime.ts
@@ -10,26 +10,26 @@ import { EventEmitter } from 'events';
 import WebSocket = require('ws');
 
 export interface FileAccessor {
-	readFile(path: string): Promise<string>;
+    readFile(path: string): Promise<string>;
 }
 
 export interface ISquidBreakpoint {
-	id: number;
-	line: number;
-	verified: boolean;
+    id: number;
+    line: number;
+    verified: boolean;
 }
 
 interface IStackFrame {
-	index: number;
-	name: string;
-	file: string;
-	line: number;
-	column?: number;
+    index: number;
+    name: string;
+    file: string;
+    line: number;
+    column?: number;
 }
 
 interface IStack {
-	count: number;
-	frames: IStackFrame[];
+    count: number;
+    frames: IStackFrame[];
 }
 
 /**
@@ -37,220 +37,220 @@ interface IStack {
  */
 export class SquidRuntime extends EventEmitter {
 
-	// the initial (and one and only) file we are 'debugging'
-	//private _sourceFile: string = '';
-	//public get sourceFile() {
-		//return this._sourceFile;
-	//}
+    // the initial (and one and only) file we are 'debugging'
+    //private _sourceFile: string = '';
+    //public get sourceFile() {
+        //return this._sourceFile;
+    //}
 
-	// Files that we've loaded lines for
-	private _sourceLines = new Map<string, string[]>();
+    // Files that we've loaded lines for
+    private _sourceLines = new Map<string, string[]>();
 
-	// This is the next line that will be 'executed'
-	//private _currentLine = 0;
-	//private _currentColumn: number | undefined;
+    // This is the next line that will be 'executed'
+    //private _currentLine = 0;
+    //private _currentColumn: number | undefined;
 
-	// maps from sourceFile to array of Squid breakpoints
-	private _breakPoints = new Map<string, ISquidBreakpoint[]>();
+    // maps from sourceFile to array of Squid breakpoints
+    private _breakPoints = new Map<string, ISquidBreakpoint[]>();
 
-	// since we want to send breakpoint events, we will assign an id to every event
-	// so that the frontend can match events with breakpoints.
-	private _breakpointId = 1;
+    // since we want to send breakpoint events, we will assign an id to every event
+    // so that the frontend can match events with breakpoints.
+    private _breakpointId = 1;
 
-	//private _breakAddresses = new Set<string>();
+    //private _breakAddresses = new Set<string>();
 
-	private _noDebug = false;
+    private _noDebug = false;
 
-	//private _namedException: string | undefined;
-	//private _otherExceptions = false;
+    //private _namedException: string | undefined;
+    //private _otherExceptions = false;
 
-	private _debuggerHostnamePort = "localhost:8000";
+    private _debuggerHostnamePort = "localhost:8000";
 
 
-	constructor(private _fileAccessor: FileAccessor) {
-		super();
-	}
+    constructor(private _fileAccessor: FileAccessor) {
+        super();
+    }
 
-	/**
-	 * Start executing the given program.
-	 */
-	public async start(hostnamePort: string, stopOnEntry: boolean, noDebug: boolean): Promise<void> {
+    /**
+     * Start executing the given program.
+     */
+    public async start(hostnamePort: string, stopOnEntry: boolean, noDebug: boolean): Promise<void> {
 
-		this._noDebug = noDebug;
+        this._noDebug = noDebug;
 
-		this._debuggerHostnamePort = hostnamePort;
-		await this.connectDebugger(this._debuggerHostnamePort).catch(_ => this.emit('end'));
-		logger.log('connected');
+        this._debuggerHostnamePort = hostnamePort;
+        await this.connectDebugger(this._debuggerHostnamePort).catch(_ => this.emit('end'));
+        logger.log('connected');
 
-		//await this.loadSource(program);
-		//this._currentLine = -1;
+        //await this.loadSource(program);
+        //this._currentLine = -1;
 
-		//await this.verifyBreakpoints(this._sourceFile);
+        //await this.verifyBreakpoints(this._sourceFile);
 
-		if (stopOnEntry) {
-			// we step once
-			this.step('stopOnEntry');
-		} else {
-			// we just start to run until we hit a breakpoint or an exception
-			this.continue();
-		}
-	}
+        if (stopOnEntry) {
+            // we step once
+            this.step('stopOnEntry');
+        } else {
+            // we just start to run until we hit a breakpoint or an exception
+            this.continue();
+        }
+    }
 
-	/**
-	 * Continue execution to the end/beginning.
-	 */
-	public continue() {
-		this.run(undefined);
-	}
+    /**
+     * Continue execution to the end/beginning.
+     */
+    public continue() {
+        this.run(undefined);
+    }
 
-	/**
-	 * Step to the next/previous non empty line.
-	 */
-	public step(event = 'stopOnStep') {
-		this.run(event);
-	}
+    /**
+     * Step to the next/previous non empty line.
+     */
+    public step(event = 'stopOnStep') {
+        this.run(event);
+    }
 
-	/**
-	 * Returns a fake 'stacktrace' where every 'stackframe' is a word from the current line.
-	 */
-	public stack(startFrame: number, endFrame: number): IStack {
-		return {
-			frames: [{
-				index: 0,
-				name: `helloWorld()`,
-				file: "hello world",
-				line: 1
-			}],
-			count: 1
-		};
-	}
+    /**
+     * Returns a fake 'stacktrace' where every 'stackframe' is a word from the current line.
+     */
+    public stack(startFrame: number, endFrame: number): IStack {
+        return {
+            frames: [{
+                index: 0,
+                name: `helloWorld()`,
+                file: "hello world",
+                line: 1
+            }],
+            count: 1
+        };
+    }
 
-	public getBreakpoints(path: string, line: number): number[] {
+    public getBreakpoints(path: string, line: number): number[] {
 
-		const l = this._sourceLines[line];
+        const l = this._sourceLines[line];
 
-		let sawSpace = true;
-		const bps: number[] = [];
-		for (let i = 0; i < l.length; i++) {
-			if (l[i] !== ' ') {
-				if (sawSpace) {
-					bps.push(i);
-					sawSpace = false;
-				}
-			} else {
-				sawSpace = true;
-			}
-		}
+        let sawSpace = true;
+        const bps: number[] = [];
+        for (let i = 0; i < l.length; i++) {
+            if (l[i] !== ' ') {
+                if (sawSpace) {
+                    bps.push(i);
+                    sawSpace = false;
+                }
+            } else {
+                sawSpace = true;
+            }
+        }
 
-		return bps;
-	}
+        return bps;
+    }
 
-	/*
-	 * Set breakpoint in file with given line.
-	 */
-	public async setBreakPoint(path: string, line: number): Promise<ISquidBreakpoint> {
+    /*
+     * Set breakpoint in file with given line.
+     */
+    public async setBreakPoint(path: string, line: number): Promise<ISquidBreakpoint> {
 
-		const bp: ISquidBreakpoint = { verified: false, line, id: this._breakpointId++ };
-		let bps = this._breakPoints.get(path);
-		if (!bps) {
-			bps = new Array<ISquidBreakpoint>();
-			this._breakPoints.set(path, bps);
-		}
-		bps.push(bp);
+        const bp: ISquidBreakpoint = { verified: false, line, id: this._breakpointId++ };
+        let bps = this._breakPoints.get(path);
+        if (!bps) {
+            bps = new Array<ISquidBreakpoint>();
+            this._breakPoints.set(path, bps);
+        }
+        bps.push(bp);
 
-		await this.verifyBreakpoints(path);
+        await this.verifyBreakpoints(path);
 
-		return bp;
-	}
+        return bp;
+    }
 
-	/*
-	 * Clear breakpoint in file with given line.
-	 */
-	public clearBreakPoint(path: string, line: number): ISquidBreakpoint | undefined {
-		const bps = this._breakPoints.get(path);
-		if (bps) {
-			const index = bps.findIndex(bp => bp.line === line);
-			if (index >= 0) {
-				const bp = bps[index];
-				bps.splice(index, 1);
-				return bp;
-			}
-		}
-		return undefined;
-	}
+    /*
+     * Clear breakpoint in file with given line.
+     */
+    public clearBreakPoint(path: string, line: number): ISquidBreakpoint | undefined {
+        const bps = this._breakPoints.get(path);
+        if (bps) {
+            const index = bps.findIndex(bp => bp.line === line);
+            if (index >= 0) {
+                const bp = bps[index];
+                bps.splice(index, 1);
+                return bp;
+            }
+        }
+        return undefined;
+    }
 
-	private async verifyBreakpoints(path: string): Promise<void> {
-		if (this._noDebug) {
-			return;
-		}
+    private async verifyBreakpoints(path: string): Promise<void> {
+        if (this._noDebug) {
+            return;
+        }
 
-		const bps = this._breakPoints.get(path);
-		if (bps) {
-			let sourceLines = await this.loadSource(path);
-			bps.forEach(bp => {
-				if (bp.line >= sourceLines.length) {
-					return;
-				}
+        const bps = this._breakPoints.get(path);
+        if (bps) {
+            let sourceLines = await this.loadSource(path);
+            bps.forEach(bp => {
+                if (bp.line >= sourceLines.length) {
+                    return;
+                }
 
-				if (!bp.verified) {
-					bp.verified = true;
-					this.sendEvent('breakpointValidated', bp);
-				}
-			});
-		}
-	}
+                if (!bp.verified) {
+                    bp.verified = true;
+                    this.sendEvent('breakpointValidated', bp);
+                }
+            });
+        }
+    }
 
-	/*
-	 * Clear all breakpoints for file.
-	 */
-	public clearBreakpoints(path: string): void {
-		this._breakPoints.delete(path);
-	}
+    /*
+     * Clear all breakpoints for file.
+     */
+    public clearBreakpoints(path: string): void {
+        this._breakPoints.delete(path);
+    }
 
-	// private methods
+    // private methods
 
-	private async connectDebugger(hostnamePort: string): Promise<void> {
-		
-		logger.log('connectDebugger');
-		var wsMessageHandler = this.onWsMessage;
-		return new Promise<void>((resolve, reject) => {
-			const ws = new WebSocket(`ws://${hostnamePort}/ws`);
-			ws.on('open', function open() {
-				ws.send("send_status");
-				resolve();
-			});
-			ws.on('message', wsMessageHandler);
-			ws.on('error', (evt: WebSocket.ErrorEvent) => {
-				logger.error(evt.message);
-				reject("Failed to connect: " + evt.message);
-			});
-		});
-	}
+    private async connectDebugger(hostnamePort: string): Promise<void> {
+        
+        logger.log('connectDebugger');
+        var wsMessageHandler = this.onWsMessage;
+        return new Promise<void>((resolve, reject) => {
+            const ws = new WebSocket(`ws://${hostnamePort}/ws`);
+            ws.on('open', function open() {
+                ws.send("send_status");
+                resolve();
+            });
+            ws.on('message', wsMessageHandler);
+            ws.on('error', (evt: WebSocket.ErrorEvent) => {
+                logger.error(evt.message);
+                reject("Failed to connect: " + evt.message);
+            });
+        });
+    }
 
-	private onWsMessage(msg: string): void {
-		logger.log("Received: " + msg);
-	}
+    private onWsMessage(msg: string): void {
+        logger.log("Received: " + msg);
+    }
 
-	private async loadSource(file: string): Promise<string[]> {
-		let sourceLines = this._sourceLines[file];
+    private async loadSource(file: string): Promise<string[]> {
+        let sourceLines = this._sourceLines[file];
 
-		if (typeof(sourceLines) == "undefined") {
-			const contents = await this._fileAccessor.readFile(file);
-			this._sourceLines[file] = contents.split(/\r?\n/);
-		}
-		return this._sourceLines[file];
-	}
+        if (typeof(sourceLines) == "undefined") {
+            const contents = await this._fileAccessor.readFile(file);
+            this._sourceLines[file] = contents.split(/\r?\n/);
+        }
+        return this._sourceLines[file];
+    }
 
-	/**
-	 * Run through the file.
-	 * If stepEvent is specified only run a single step and emit the stepEvent.
-	 */
-	private run(stepEvent?: string) {
-	}
+    /**
+     * Run through the file.
+     * If stepEvent is specified only run a single step and emit the stepEvent.
+     */
+    private run(stepEvent?: string) {
+    }
 
-	private sendEvent(event: string, ... args: any[]) {
-		setImmediate(_ => {
-			this.emit(event, ...args);
-		});
-	}
+    private sendEvent(event: string, ... args: any[]) {
+        setImmediate(_ => {
+            this.emit(event, ...args);
+        });
+    }
 }
